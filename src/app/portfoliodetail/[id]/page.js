6f0638f2d1289de9b44d41136ecbff0e2370c5eb
adriano <adriano.gratani1@gmail.com>
2024-09-18 18:20:35 +0900

    d

--- src/app/portfoliodetail/[id]/page.js
+++ src/app/portfoliodetail/[id]/page.js
@@ -3,9 +3,13 @@ import { DataArray } from "@/app/data"
 import React from 'react'
 import PortfolioDetailDesign from "../../page/portfolio-detail-design"
 
-
+import {useEffect} from 'react';
 const Portfolio = (props)=> {
     // console.log(props.params.id)
+
+    useEffect(()=> {
+        document.title = "Projects | Adriano Gratani"
+    },[])
     return(
         <React.Fragment>
             <PortfolioDetailDesign   
